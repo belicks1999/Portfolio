ad055c333afb0a4b78110adfbe5313f72a5beb81
Belicks <Belicks@LAPTOP-4LF71QVV>
2024-07-09 20:46:13 +0530

    fix bug

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -113,52 +113,57 @@ const Project = () => {
     };
 
     return (
-        <motion.div ref={ref} initial={{ opacity: 0, y: 20 }} animate={controls} className="border-b border-neutral-800 container mx-auto px-4 pb-24 lg:px-8">
-            <motion.h1
-                initial={{ opacity: 0, y: -20 }}
-                animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: -20 }}
-                transition={{ duration: 0.6 }}
-                className="my-24 text-center text-3xl lg:text-4xl"
-            >
-                Projects
-            </motion.h1>
-            {loading && <p className="text-center text-white">Loading projects...</p>}
-            {error && <p className="text-center text-red-500">{error}</p>}
-            {!loading && !error && (
-                <Slider {...settings}>
-                    {projects.map((project, index) => (
-                        <motion.div
-                            key={project.id}
-                            initial={{ opacity: 0, y: 20 }}
-                            animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
-                            transition={{ duration: 0.6, delay: index * 0.1 }}
-                            className="px-2 lg:px-4"
-                        >
-                            <div className="shadow-lg rounded-lg overflow-hidden h-full pb-16 w-full lg:h-96 relative bg-gray-800 bg-opacity-50 backdrop-blur-lg"> 
-                                    <div className="p-4 lg:p-6 h-full flex flex-col justify-between">
-                                        <div>
-                                            <h2 className="text-white text-center text-xl lg:text-2xl font-bold mb-4 lg:mb-6">{project.name}</h2>
-                                            <p className="text-gray-300 text-center text-sm lg:text-base mb-4 lg:mb-4">{project.description}</p>
-                                        </div>
-                                        <div className="flex-grow"></div>
-                                        <div className="text-center mt-4 lg:mt-3">
-                                            <div className="text-gray-400 text-center mb-4 lg:mb-6">
-                                                <p>Languages: {project.languages.join(', ')}</p>
-                                            </div>
-                                            <a href={project.html_url} target="_blank" rel="noopener noreferrer" className="block">
-                                            <button className="bg-blue-800 text-white rounded-lg py-2 px-4 transition-all duration-300 hover:bg-blue-700">GitHub</button>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                             
-
-                        </motion.div>
-                    ))}
-                </Slider>
-            )}
+        <motion.div
+          ref={ref}
+          initial={{ opacity: 0, y: 20 }}
+          animate={controls}
+          className="border-b border-neutral-800 container mx-auto px-4 pb-24 lg:px-8"
+        >
+          <motion.h1
+            initial={{ opacity: 0, y: -20 }}
+            animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: -20 }}
+            transition={{ duration: 0.6 }}
+            className="my-24 text-center text-3xl lg:text-4xl"
+          >
+            Projects
+          </motion.h1>
+          {loading && <p className="text-center text-white">Loading projects...</p>}
+          {error && <p className="text-center text-red-500">{error}</p>}
+          {!loading && !error && (
+            <Slider {...settings}>
+              {projects.map((project, index) => (
+                <motion.div
+                  key={project.id}
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+                  transition={{ duration: 0.6, delay: index * 0.1 }}
+                  className="px-2 lg:px-4 relative" // Add relative positioning here
+                >
+                  <div className="shadow-lg rounded-lg overflow-hidden h-full pb-16 w-full lg:h-96 relative bg-gray-800 bg-opacity-50 backdrop-blur-lg">
+                    <div className="p-4 lg:p-6 h-full flex flex-col justify-between">
+                      <div>
+                        <h2 className="text-white text-center text-xl lg:text-2xl font-bold mb-4 lg:mb-6">{project.name}</h2>
+                        <p className="text-gray-300 text-center text-sm lg:text-base mb-4 lg:mb-4">{project.description}</p>
+                      </div>
+                      <div className="flex-grow"></div>
+                      <div className="absolute bottom-4 left-0 right-0"> {/* Positioning for fixed bottom */}
+                        <div className="text-center mb-4 lg:mb-6">
+                          <p className="text-gray-400">Languages: {project.languages.join(', ')}</p>
+                        </div>
+                        <div className="text-center">
+                          <a href={project.html_url} target="_blank" rel="noopener noreferrer" className="block">
+                            <button className="bg-indigo-600 hover:bg-indigo-700 text-white rounded-lg py-2 px-4 transition-all duration-300 ">GitHub</button>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </motion.div>
+              ))}
+            </Slider>
+          )}
         </motion.div>
-    );
+      );
 };
 
 export default Project;
