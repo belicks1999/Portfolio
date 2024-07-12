f9d3012b8af567da8caae6bba65c0025b969424d
Belicks <Belicks@LAPTOP-4LF71QVV>
2024-07-12 15:13:50 +0530

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
   return (
     <div className="h-screen overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       {/* Background elements */}
-      <div className="fixed top-0 left-0 right-0 bottom-0 z-[-10]"></div>
+      <div className="fixed top-0 left-0 right-0 bottom-0 z-[-10] h-full w-full"></div>
       <div className="absolute top-0 left-0 right-0 bottom-0 z-[-2] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
       
       {/* Main content */}
@@ -31,7 +31,7 @@ const App = () => {
         {loading ? (
           <Loaders />
         ) : (
-          <div className="container mx-auto px-16  h-full">
+          <div className="container mx-auto px-16 ">
             <Navbar />
             <Hero />
             <About />
