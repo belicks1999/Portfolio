8938300f24090cd09ad4f4c63a9a308811238164
Belicks <Belicks@LAPTOP-4LF71QVV>
2024-07-12 15:19:20 +0530

    reverse

--- src/App.jsx
+++ src/App.jsx
@@ -22,16 +22,15 @@ const App = () => {
 
   return (
     <div className="h-screen overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      {/* Background elements */}
-      <div className="fixed top-0 left-0 right-0 bottom-0 z-[-10] h-full w-full"></div>
-      <div className="absolute top-0 left-0 right-0 bottom-0 z-[-2] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
-      
-      {/* Main content */}
-      <div className="relative h-full">
+      <div className="fixed top-0 z-[-10] h-full w-full"></div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+      <div className="container mx-auto px-16">
         {loading ? (
-          <Loaders />
+          
+          <Loaders className="justify-center items-center" /> 
+         
         ) : (
-          <div className="container mx-auto px-16 ">
+          <>
             <Navbar />
             <Hero />
             <About />
@@ -40,7 +39,7 @@ const App = () => {
             <Contact />
             <Download />
             <Footer />
-          </div>
+          </>
         )}
       </div>
     </div>
