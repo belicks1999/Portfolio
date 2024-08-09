8c9caed0af7a0a7c24d3bc0f35e99004b9e39295
Belicks <Belicks@LAPTOP-4LF71QVV>
2024-08-09 20:01:56 +0530

    bug fixes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,8 +6,6 @@ import { useInView } from 'react-intersection-observer';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-
-
 const Project = () => {
     const [projects, setProjects] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -15,7 +13,7 @@ const Project = () => {
 
     const controls = useAnimation();
     const [ref, inView] = useInView({
-        triggerOnce: false, // Ensures the animation works every time it comes into view
+        triggerOnce: false,
     });
 
     useEffect(() => {
@@ -37,9 +35,7 @@ const Project = () => {
         const fetchProjects = async () => {
             try {
                 const GITHUB_USERNAME = 'belicks1999';
-                
 
-                // Fetch repositories for the GitHub user
                 const response = await fetch(`https://api.github.com/users/${GITHUB_USERNAME}/repos`);
 
                 if (!response.ok) {
@@ -48,7 +44,6 @@ const Project = () => {
 
                 const projectsData = await response.json();
 
-                // Fetch languages for each project
                 const projectsWithLanguages = await Promise.all(
                     projectsData.map(async (project) => {
                         const languagesResponse = await axios.get(project.languages_url);
@@ -137,28 +132,32 @@ const Project = () => {
                   initial={{ opacity: 0, y: 20 }}
                   animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
                   transition={{ duration: 0.6, delay: index * 0.1 }}
-                  className="px-2 lg:px-4 relative" // Add relative positioning here
+                  className="px-2 lg:px-4 relative"
                 >
-                  <div  className="shadow-lg rounded-lg overflow-hidden h-[540px] pb-16 w-full lg:h-96 relative bg-gray-800 bg-opacity-50 backdrop-blur-lg">
+                  <div className="shadow-lg rounded-lg overflow-hidden h-[540px] pb-16 w-full lg:h-96 relative bg-gray-800 bg-opacity-50 backdrop-blur-lg">
                     <div className="p-4 lg:p-6 h-full flex flex-col justify-between">
                       <div>
                         <h2 className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 text-center text-xl lg:text-2xl font-bold mb-4 lg:mb-6">{project.name}</h2>
                         <p className="text-gray-300 text-center text-sm lg:text-base mb-12 lg:mb-4">{project.description}</p>
                       </div>
                       <div className="flex-grow"></div>
-                      <div className="absolute bottom-4 left-0 right-0 "> {/* Positioning for fixed bottom */}
+                      <div className="absolute bottom-4 left-0 right-0"> {/* Positioning for fixed bottom */}
                         <div className="text-center sm:mt-10 mb-4 lg:mb-6">
                           <p className="text-gray-400 text-sm lg:text-lg">Languages: {project.languages.join(', ')}</p>
                         </div>
-                        <div className="text-center flex flex-col ">
-                          <a href={project.html_url} target="_blank" rel="noopener noreferrer" className="block">
-                            <button className="bg-indigo-600 hover:bg-indigo-700 text-white rounded-lg py-2 px-4 transition-all duration-300 lg:w-28 ">GitHub</button>
-                            {project.homepage && (
-                          <a href={project.homepage} target="_blank" rel="noopener noreferrer" className="block mt-2">
-                            <button className="bg-green-600 hover:bg-green-700 text-white rounded-lg py-2 px-4 transition-all duration-300 lg:w-28">Live Demo</button>
-                      </a>
-                    )}
+                        <div className="text-center flex flex-col sm:flex-row sm:justify-center sm:gap-4">
+                          <a href={project.html_url} target="_blank" rel="noopener noreferrer">
+                            <button className="bg-indigo-600 hover:bg-indigo-700 text-white rounded-lg py-2 px-4 transition-all duration-300 lg:w-28">
+                              GitHub
+                            </button>
                           </a>
+                          {project.homepage && (
+                            <a href={project.homepage} target="_blank" rel="noopener noreferrer">
+                              <button className="bg-green-600 hover:bg-green-700 text-white rounded-lg py-2 px-4 mt-2 sm:mt-0 transition-all duration-300 lg:w-28">
+                                Live Demo
+                              </button>
+                            </a>
+                          )}
                         </div>
                       </div>
                     </div>
